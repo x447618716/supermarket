bf91ea05f964292584fbee47b9149ea330f7ba17
xierongsheng <447618716@qq.com>
2025-10-21 15:50:22 +0800

    refactor: 删除未使用的 requestMap 和相关函数

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -6,22 +6,6 @@ import type { BaseResponse } from '@/services/vo/conmmon';
 import { checkStatus } from '@/utils/http/checkStatus';
 
 /**
- * 请求内置操作
- * */
-interface RequestOperate {
-    /**
-     * 请求地址集
-     * key:请求地址
-     * value:请求时间戳
-     * */
-    requestMap: Map<string, number>;
-}
-
-const requestOperate: RequestOperate = {
-    requestMap: new Map<string, number>()
-};
-
-/**
  * alova 请求实例
  * @link https://github.com/alovajs/alova
  */
@@ -32,16 +16,6 @@ const alovaInstance = createAlova({
     cacheFor: null, // 完全禁用缓存
     beforeRequest: async method => {
         const authStore = useAuthStore();
-        timerRecycling();
-        //判断当前请求是否已在请求列表中
-        if (requestOperate.requestMap.has(method.url)) {
-            method.abort();
-        } else {
-            //判断是否加入请求集-> 默认为加入
-            if (method.meta?.debounce ?? true) {
-                requestOperate.requestMap.set(method.url, new Date().getTime());
-            }
-        }
         method.config.headers = assign(authStore.getAuthorization, { apifoxToken: 'zC_7kbY3_WeqXhxSog7ry' }, method.config.headers);
     },
     responded: {
@@ -84,22 +58,8 @@ const alovaInstance = createAlova({
         onError: (err: object, method) => {
             Toast(err.toString());
             return Promise.reject({ err, method });
-        },
-        onComplete: methods => {
-            //删除指定项
-            requestOperate.requestMap.delete(methods.url);
         }
     }
 });
-
-//回收
-const timerRecycling = () => {
-    requestOperate.requestMap.forEach((value, key, map) => {
-        //请求列中项存在时间超1分钟将其清除
-        if (new Date().getTime() - value > NumberEnum.TIME_OUT) {
-            map.delete(key);
-        }
-    });
-};
 
 export default alovaInstance;
